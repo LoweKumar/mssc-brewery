3721ff921dbf752fb48471bd5b0a6dff13fa095c
LoweKumar <lowekr00@gmail.com>
2022-05-31 22:09:01 +0530

    Section4-Lecture31

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,17 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
 
         return new ResponseEntity<>(beerService.getBeerById(beerId),HttpStatus.OK);
+
+    }
+
+    public ResponseEntity handlePost(BeerDto beerDto)
+    {
+        BeerDto saveDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //todo add hostname to url
+        headers.add("Location","/api/v1/beer"+saveDto.getId().toString());
+        return new ResponseEntity(headers,HttpStatus.CREATED);
 
     }
 }
